19ca3e7d0a6e2f70c7a88fe66bb2cfd3cd59dfe1
yukiya <yukiya20021128@icloud.com>
2024-06-28 22:34:23 +0900

    created-all

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,13 @@
 from rest_framework import routers
 from django.urls import path
 from django.conf.urls import include
+from .views import CreateUserView, VideoViewSet
 
 
 router = routers.DefaultRouter()
+router.register('videos', VideoViewSet)
 
 urlpatterns = [
+    path('create/', CreateUserView.as_view(), name='create'),
     path('',include(router.urls)),
 ]
-
-
-def serializers():
-    return 1
--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets, generics
+from .seriarizers import VideoSerializer, UserSerializer
+from rest_framework.permissions import AllowAny
+from .models import Video
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+
+class VideoViewSet(viewsets.ModelViewSet):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer

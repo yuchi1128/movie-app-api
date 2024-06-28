a95ac42fade4d5e3f776d0e1d97662b7687eaba1
yukiya <yukiya20021128@icloud.com>
2024-06-28 20:44:26 +0900

    first commit

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from rest_framework import routers
+from django.urls import path
+from django.conf.urls import include
+
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('',include(router.urls)),
+]
